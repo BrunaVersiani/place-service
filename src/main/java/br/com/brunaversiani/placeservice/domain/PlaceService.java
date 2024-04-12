d0e6f021ddb562111ad4b5f11e254b43b298de29
raytechx <rayaneversiani@gmail.com>
2024-04-12 00:59:31 -0300

    configuracao slugify

--- src/main/java/br/com/brunaversiani/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/brunaversiani/placeservice/domain/PlaceService.java
@@ -1,19 +1,22 @@
 package br.com.brunaversiani.placeservice.domain;
 
 import br.com.brunaversiani.placeservice.api.PlaceRequest;
+import com.github.slugify.Slugify;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slugify;
 
-    public PlaceService(PlaceRepository placeRepository){
+    public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slugify = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(),
+        var place = new Place(null, placeRequest.name(), slugify.slugify(placeRequest.name()),
                 placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
         return placeRepository.save(place);
     }
